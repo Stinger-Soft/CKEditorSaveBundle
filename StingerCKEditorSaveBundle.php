ff56d24fca88a3ec64b8075609dbfd074a92b33e
koddistortion <oliver.kotte@stinger-soft.net>
2015-09-29 11:49:57 +0200

    initial commit of CKEditor Save plugin v4.5.3

---
+++ StingerCKEditorSaveBundle.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: oliver.kotte
+ * Date: 29.09.2015
+ * Time: 11:46
+ */
+
+namespace Stinger\CKEditorSaveBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class StingerCKEditorSaveBundle extends Bundle {
+
+}
